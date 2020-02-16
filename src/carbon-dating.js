3273cf9e5d2a63a9af5c2024be341b1933383215
ROnic404 <ronic@inbox.ru>
2020-02-16 19:03:19 +0400

    feat: add solution subtask carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,9 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const activity = parseFloat(sampleActivity);
+  if (typeof sampleActivity !== 'string') return false;  
+  if (activity <= 0 || activity > MODERN_ACTIVITY || Number.isNaN(activity)) return false;  
+  return Math.ceil(Math.log(MODERN_ACTIVITY/activity) / (0.693/HALF_LIFE_PERIOD));
 };
