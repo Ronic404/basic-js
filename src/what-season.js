5a6d548a152a230750a495d3c74711d0074a0459
ROnic404 <ronic@inbox.ru>
2020-02-23 16:43:22 +0400

    feat: add solution subtask what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,13 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {  
+  let season = '';
+  if(date === undefined) return "Unable to determine the time of year!";
+  if({}.toString.call(date) != '[object Date]') throw new Error;
+
+  const month = date.getMonth();    
+  (month <= 1 || month === 11) ? season = 'winter' :
+  (month >= 2 && month <= 4) ? season = 'spring' :
+  (month >= 5 && month <= 7) ? season = 'summer' :
+  (month >= 8 && month <= 10) ? season = 'autumn' : season = 'Error';
+
+  return season;
 };
