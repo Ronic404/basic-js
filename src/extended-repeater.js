5049292488878a32204e76cfd09a0eca2dc7b943
ROnic404 <ronic@inbox.ru>
2020-02-25 22:17:39 +0400

    feat: add solution subtask extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,30 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let arrResult = [];    
+    let addArray = []; 
+    let addString = '';      
+    if(options.addition === false) options.addition = 'false';
+    if(options.addition === null) options.addition = 'null';
+
+    if(options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1; // additionRepeatTimes
+    for(let i = 0; i < options.additionRepeatTimes; i++) { 
+        addArray.push(options.addition);
+    }
+    
+    if(options.additionRepeatTimes && options.additionSeparator) { // additionSeparator
+        addString = addArray.join(options.additionSeparator); 
+    }     
+        
+    if(options.additionSeparator) { // addition
+        str += addString;
+    } else if(options.addition)  {
+        str += options.addition;
+    } 
+
+    if(options.repeatTimes === undefined) options.repeatTimes = 1; // repeatTimes
+    for(let i = 0; i < options.repeatTimes; i++) { 
+        arrResult.push(str);
+    }
+
+    if(options.separator === undefined) options.separator = '+'; // separator
+    return arrResult.join(options.separator);
 };
-  
