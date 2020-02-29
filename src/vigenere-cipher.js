07cb97c720f28a0fdba82f0bd0759c43dacf61da
ROnic404 <ronic@inbox.ru>
2020-02-29 15:15:06 +0400

    feat: add solution subtask vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,54 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(boolean) {
+        this.boolean = boolean;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if (!message || !key) throw new Error;
+
+        message = message.toUpperCase();
+        if(this.boolean === false) message = message.split('').reverse().join('');
+        key = key.toUpperCase();
+        while(key.length < message.length) key += key;      
+        let result = '';
+        let keyIndex = 0;
+        
+        for(let i of message) {
+            if(i.charCodeAt() >= 65 && i.charCodeAt() <= 90) {
+                let letter = i.charCodeAt() + key.charCodeAt(keyIndex) - 65;
+                if(letter > 90) letter -= 26;
+                result += String.fromCharCode(letter);
+                keyIndex++;
+            } else {
+                result += i;
+            }
+        }
+
+        return result;
+    }
+
+    decrypt(message, key) {
+        if (!message || !key) throw new Error;
+
+        message = message.toUpperCase();
+        if(this.boolean === false) message = message.split('').reverse().join('');
+        key = key.toUpperCase();
+        while(key.length < message.length) key += key;      
+        let result = '';
+        let keyIndex = 0;
+        
+        for(let i of message) {
+            if(i.charCodeAt() >= 65 && i.charCodeAt() <= 90) {
+                let letter = i.charCodeAt() - key.charCodeAt(keyIndex) + 65;
+                if(letter < 65) letter += 26;
+                result += String.fromCharCode(letter);
+                keyIndex++;
+            } else {
+                result += i;
+            }
+        }
+
+        return result;
     }
 }
 
